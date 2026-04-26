d275caa9409aaa1df28272841587ad118eca0687
Rakotoarivelo_Sedera <contact.sederaj@gmail.com>
2026-04-26 13:28:22 +0300

    feat: paginated tournament table

--- sportslife-client/src/app/features/tournaments/pages/tournament-list/tournaments-list-page.component.ts
+++ sportslife-client/src/app/features/tournaments/pages/tournament-list/tournaments-list-page.component.ts
@@ -5,11 +5,13 @@ import { DatePipe } from '@angular/common';
 import { Tournament } from '../../../../core/models/tournament.model';
 import { AuthService } from '../../../../core/services/auth.service';
 import { TournamentService } from '../../../../core/services/tournament.service';
+import { PageEvent } from '@angular/material/paginator';
+import { TournamentTableComponent } from '../../components/tournament-table/tournament-table.component';
 
 @Component({
   selector: 'app-tournaments-list-page',
   standalone: true,
-  imports: [RouterLink, DatePipe],
+  imports: [RouterLink, TournamentTableComponent],
   templateUrl: './tournaments-list-page.component.html',
   styleUrl: './tournaments-list-page.component.scss',
 })
@@ -17,6 +19,9 @@ export class TournamentsListPageComponent implements OnInit {
   readonly tournaments = signal<Tournament[]>([]);
   readonly loading = signal(false);
   readonly error = signal<string | null>(null);
+  totalItems = signal(0);
+  pageIndex = signal(0);
+  pageSize = signal(10);
 
   constructor(
     private readonly tournamentService: TournamentService,
@@ -32,9 +37,13 @@ export class TournamentsListPageComponent implements OnInit {
     this.loading.set(true);
     this.error.set(null);
 
-    this.tournamentService.getTournaments().subscribe({
+    const page = this.pageIndex() + 1;
+    const limit = this.pageSize();
+
+    this.tournamentService.getTournaments(page, limit).subscribe({
       next: (response) => {
         this.tournaments.set(response.data.items);
+        this.totalItems.set(response.data?.meta?.total);
         this.loading.set(false);
       },
       error: (error) => {
@@ -42,6 +51,12 @@ export class TournamentsListPageComponent implements OnInit {
         this.loading.set(false);
       },
     });
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.pageIndex.set(event.pageIndex);
+    this.pageSize.set(event.pageSize);
+    this.fetchTournaments();
   }
 
   goToCreate(): void {
